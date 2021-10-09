6820a95e6ae8e92ebd76633967c70141ee65dc4c
kijikunnn <unicast.trial.b@unicast.ne.jp>
2021-10-09 09:27:46 +0900

    💄 update css to tailwindcss

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -18,7 +18,7 @@ const UserInfo = () => {
     return (
       <>
         <button
-          className="button small"
+          className="text-base bg-[#6700eb] px-6 py-3 text-[#f4f4f4] text-center hover:bg-[#45009d]"
           onClick={async () => {
             await logoutMutation()
           }}
@@ -36,12 +36,12 @@ const UserInfo = () => {
     return (
       <>
         <Link href={Routes.SignupPage()}>
-          <a className="button small">
+          <a className="text-base bg-[#6700eb] px-6 py-3 text-[#f4f4f4] text-center hover:bg-[#45009d]">
             <strong>Sign Up</strong>
           </a>
         </Link>
         <Link href={Routes.LoginPage()}>
-          <a className="button small">
+          <a className="text-base bg-[#6700eb] px-6 py-3 text-[#f4f4f4] text-center hover:bg-[#45009d]">
             <strong>Login</strong>
           </a>
         </Link>
@@ -52,49 +52,20 @@ const UserInfo = () => {
 
 const Home: BlitzPage = () => {
   return (
-    <div className="container">
-      <main>
-        <div className="logo">
+    <div className="min-h-screen flex justify-center flex-col items-center">
+      <main className="py-20 flex-1 flex flex-col justify-center items-center">
+        <div className="mb-8 w-80">
           <Image src={logo} alt="blitzjs" />
         </div>
-        <p>
-          <strong>Congrats!</strong> Your app is ready, including user sign-up and log-in.
-        </p>
-        <div className="buttons" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+        <p className="text-xl mb-8">This is Blitz.js&#39;s starter code</p>
+        <div className="mb-12 grid grid-flow-col gap-2">
           <Suspense fallback="Loading...">
             <UserInfo />
           </Suspense>
         </div>
-        <p>
-          <strong>
-            To add a new model to your app, <br />
-            run the following in your terminal:
-          </strong>
-        </p>
-        <pre>
-          <code>blitz generate all project name:string</code>
-        </pre>
-        <div style={{ marginBottom: "1rem" }}>(And select Yes to run prisma migrate)</div>
-        <div>
-          <p>
-            Then <strong>restart the server</strong>
-          </p>
-          <pre>
-            <code>Ctrl + c</code>
-          </pre>
-          <pre>
-            <code>blitz dev</code>
-          </pre>
-          <p>
-            and go to{" "}
-            <Link href="/projects">
-              <a>/projects</a>
-            </Link>
-          </p>
-        </div>
-        <div className="buttons" style={{ marginTop: "5rem" }}>
+        <div className=" grid grid-flow-col gap-2">
           <a
-            className="button"
+            className="text-base bg-[#6700eb] py-4 px-8 text-[#f4f4f4] text-center hover:bg-[#45009d]"
             href="https://blitzjs.com/docs/getting-started?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
             target="_blank"
             rel="noopener noreferrer"
@@ -102,7 +73,7 @@ const Home: BlitzPage = () => {
             Documentation
           </a>
           <a
-            className="button-outline"
+            className="text-base  py-4 px-8 text-[#6700eb] text-center border-2 border-[#6700eb] hover:text-[#45009d] hover:border-[#45009d]"
             href="https://github.com/blitz-js/blitz"
             target="_blank"
             rel="noopener noreferrer"
@@ -110,7 +81,7 @@ const Home: BlitzPage = () => {
             Github Repo
           </a>
           <a
-            className="button-outline"
+            className="text-base  py-4 px-8 text-[#6700eb] text-center border-2 border-[#6700eb] hover:text-[#45009d] hover:border-[#45009d]"
             href="https://discord.blitzjs.com"
             target="_blank"
             rel="noopener noreferrer"
@@ -119,9 +90,9 @@ const Home: BlitzPage = () => {
           </a>
         </div>
       </main>
-
-      <footer>
+      <footer className="w-full h-16 border-t border-[#eaeaea] flex justify-center items-center bg-[#45009d]">
         <a
+          className="flex justify-center items-center text-[#f4f4f4] no-underline"
           href="https://blitzjs.com?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
           target="_blank"
           rel="noopener noreferrer"
@@ -129,139 +100,6 @@ const Home: BlitzPage = () => {
           Powered by Blitz.js
         </a>
       </footer>
-
-      <style jsx global>{`
-        @import url("https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;700&display=swap");
-
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: "Libre Franklin", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-          box-sizing: border-box;
-        }
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main p {
-          font-size: 1.2rem;
-        }
-
-        p {
-          text-align: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 60px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #45009d;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer a {
-          color: #f4f4f4;
-          text-decoration: none;
-        }
-
-        .logo {
-          margin-bottom: 2rem;
-        }
-
-        .logo img {
-          width: 300px;
-        }
-
-        .buttons {
-          display: grid;
-          grid-auto-flow: column;
-          grid-gap: 0.5rem;
-        }
-        .button {
-          font-size: 1rem;
-          background-color: #6700eb;
-          padding: 1rem 2rem;
-          color: #f4f4f4;
-          text-align: center;
-        }
-
-        .button.small {
-          padding: 0.5rem 1rem;
-        }
-
-        .button:hover {
-          background-color: #45009d;
-        }
-
-        .button-outline {
-          border: 2px solid #6700eb;
-          padding: 1rem 2rem;
-          color: #6700eb;
-          text-align: center;
-        }
-
-        .button-outline:hover {
-          border-color: #45009d;
-          color: #45009d;
-        }
-
-        pre {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          text-align: center;
-        }
-        code {
-          font-size: 0.9rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
     </div>
   )
 }
